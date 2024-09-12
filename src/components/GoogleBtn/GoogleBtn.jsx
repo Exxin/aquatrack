4544922d7e4abe7d93982f55a45c564e1093f54a
Exxin <exxinblood@gmail.com>
2024-09-12 16:49:31 +0300

    Update GoogleBtn.jsx

--- src/components/GoogleBtn/GoogleBtn.jsx
+++ src/components/GoogleBtn/GoogleBtn.jsx
@@ -4,24 +4,24 @@ import { FcGoogle } from 'react-icons/fc';
 import css from './GoogleBtn.module.css';
 
 const GoogleBtn = () => {
-const handleGoogleLogin = () => {
-  const clientId = '1023602385270-fvjc183aqggcs1q49l8090rtjqg2nvus.apps.googleusercontent.com';
-  const redirectUri = 'https://aquatrack-one.vercel.app/auth/google/callback';
-  const scope = 'email profile';
-  const responseType = 'code';
-  const accessType = 'offline';
-  const includeGrantedScopes = 'true';
+  const handleGoogleLogin = () => {
+    const clientId = '1023602385270-fvjc183aqggcs1q49l8090rtjqg2nvus.apps.googleusercontent.com';
+    const redirectUri = 'https://test-aquatrack-backend.onrender.com/auth/google/callback';
+    const scope = 'email profile';
+    const responseType = 'code';
+    const accessType = 'offline';
+    const includeGrantedScopes = 'true';
 
-  const authUrl = `https://accounts.google.com/o/oauth2/v2/auth?` +
-    `client_id=${encodeURIComponent(clientId)}&` +
-    `redirect_uri=${encodeURIComponent(redirectUri)}&` +
-    `scope=${encodeURIComponent(scope)}&` +
-    `response_type=${encodeURIComponent(responseType)}&` +
-    `access_type=${encodeURIComponent(accessType)}&` +
-    `include_granted_scopes=${encodeURIComponent(includeGrantedScopes)}`;
+    const authUrl = `https://accounts.google.com/o/oauth2/v2/auth?` +
+      `client_id=${encodeURIComponent(clientId)}&` +
+      `redirect_uri=${encodeURIComponent(redirectUri)}&` +
+      `scope=${encodeURIComponent(scope)}&` +
+      `response_type=${encodeURIComponent(responseType)}&` +
+      `access_type=${encodeURIComponent(accessType)}&` +
+      `include_granted_scopes=${encodeURIComponent(includeGrantedScopes)}`;
 
-  window.location.href = authUrl;
-};
+    window.location.href = authUrl;
+  };
 
   return (
     <button className={css.googleBtn} onClick={handleGoogleLogin}>
