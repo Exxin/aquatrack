067b7975b67928dd2141d6aef7caaa3cba9c5a25
Exxin <exxinblood@gmail.com>
2024-09-12 16:26:05 +0300

    Update GoogleBtn.jsx

--- src/components/GoogleBtn/GoogleBtn.jsx
+++ src/components/GoogleBtn/GoogleBtn.jsx
@@ -4,8 +4,14 @@ import { FcGoogle } from 'react-icons/fc';
 import css from './GoogleBtn.module.css';
 
 const GoogleBtn = () => {
-  const handleGoogleLogin = async () => {
-    window.location.href = 'https://test-aquatrack-backend.onrender.com/auth/google'; // Redirect to backend for Google auth
+  const handleGoogleLogin = () => {
+    window.location.href = 'https://accounts.google.com/o/oauth2/v2/auth?' +
+      'client_id=YOUR_CLIENT_ID&' +
+      'redirect_uri=https://project-water-bac.onrender.com/auth/google/callback&' +
+      'scope=email profile&' +
+      'response_type=code&' +
+      'access_type=offline&' +
+      'include_granted_scopes=true';
   };
 
   return (
